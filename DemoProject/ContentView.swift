56a1e6c86e62b781165a445728bf86652ae75116
chandanirudani <chandanirudani@gmail.com>
2024-03-20 16:51:27 +0530

    c2

--- DemoProject/ContentView.swift
+++ DemoProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("commit1")
+            Text("commit2")
         }
         .padding()
     }
