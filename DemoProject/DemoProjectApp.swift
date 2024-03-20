b9efc296a17769fee7239ec2146b5cc0413f6ebe
chandanirudani <chandanirudani@gmail.com>
2024-03-20 11:10:28 +0530

    Initial Commit

---
+++ DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by Chandani Rudani on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
